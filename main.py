991fdf7f451f71f2933e8865f4db6ddde371f38c
Lucian <luciangeorge06@yahoo.com>
2024-04-20 00:43:59 +0300

    sent and stared pages

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ app.mount("/static", StaticFiles(directory="templates"), name="static")
 @app.get("/inbox", response_class=FileResponse)
 def read_login():
     return "templates/inbox_page.html"
+
+@app.get("/sent", response_class=FileResponse)
+def read_login():
+    return "templates/sent_page.html"
+
+@app.get("/stared", response_class=FileResponse)
+def read_login():
+    return "templates/stared_page.html"
