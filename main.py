623b6605d2314c432b93f80a0b06377b830f0ea1
Lucian <luciangeorge06@yahoo.com>
2024-05-03 21:12:06 +0300

    connected with database

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from database import SessionLocal, engine
+from models import Email, Base
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="templates"), name="static")
---
+++ models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+class UserInDb(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String, index=True)
+    disabled = Column(Boolean)
+
+class Email(Base):
+    __tablename__ = "emails"
+    
+    id = Column(Integer, primary_key=True)
+    sender = Column(String, index=True)
+    recipient = Column(String, index=True)
+    subject = Column(String, index=True)
+    message = Column(String, index=True)
+    starred = Column(Boolean)
+    read = Column(Boolean)
