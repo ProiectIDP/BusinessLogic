d887870c679bc1c859d5e3ae1ddddf4740788892
Lucian <luciangeorge06@yahoo.com>
2024-05-07 23:14:49 +0300

    email app almost done

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from database import SessionLocal, engine
+from sqlalchemy.orm import Session
+from sqlalchemy import or_
 from models import Email, Base
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 def get_db():
@@ -11,6 +14,12 @@ def get_db():
         yield db
     finally:
         db.close()
+
+class EmailForm(BaseModel):
+    sender: str
+    recipient: str
+    subject: str
+    message: str
 
 Base.metadata.create_all(bind=engine)
 
@@ -24,6 +33,45 @@ app.add_middleware(
     allow_headers=["Authorization", "Content-Type"],
 )
 
+
+@app.get("/getinbox")
+def get_inbox_emails(email: str, db: Session = Depends(get_db)):
+    return db.query(Email).filter(Email.recipient == email).all()
+
+@app.get("/getsent")
+def get_sent_emails(email: str, db: Session = Depends(get_db)):
+    return db.query(Email).filter(Email.sender == email).all()
+
+@app.get("/getstared")
+def get_stared_emails(email: str, db: Session = Depends(get_db)):
+    return db.query(Email).filter(or_(Email.sender == email, Email.recipient == email), Email.starred == True).all()
+
+@app.post("/starunstar")
+async def star_unstar_email(email_id: int, db: Session = Depends(get_db)):
+    email = db.query(Email).filter(Email.id == email_id).first()
+    if email:
+        setattr(email, "starred", not email.starred)
+        db.commit()
+        return {"message": "Email updated successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Email not found")
+
+@app.post("/reademail")
+async def read_email(email_id: int, db: Session = Depends(get_db)):
+    email = db.query(Email).filter(Email.id == email_id).first()
+    if email:
+        setattr(email, "read", True)
+        db.commit()
+        return {"message": "Email updated successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Email not found")
+
+@app.post("/sendemail")
+async def send_email(form_data: EmailForm, db: Session = Depends(get_db)):
+    email = Email(sender=form_data.sender, recipient=form_data.recipient, subject=form_data.subject, message=form_data.message, read=False, starred=False)
+    db.add(email)
+    db.commit()
+    return {"message": "succes"}
 
 @app.get("/inbox", response_class=FileResponse)
 def read_login():
